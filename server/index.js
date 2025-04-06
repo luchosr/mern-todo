276b43451028635140406f21ccd146c46fae9454
Luciano Ramello <luchosr@gmail.com>
2025-04-06 20:58:45 +0200

    feat(server): instantiates express app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log('Server is running');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
